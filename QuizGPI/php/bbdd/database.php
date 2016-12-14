1c36c01a445b4f5a0c8a4b2aef24c37893c066e8
Vykstorm <victorruizgomezdev@gmail.com>
2016-12-14 18:09:55 +0100

    Eliminado codigo de depuracion para acceder a la bd

--- QuizGPI/php/bbdd/database.php
+++ QuizGPI/php/bbdd/database.php
@@ -9,7 +9,7 @@ class DataBase
 	// Conexion con la base de datos
 	protected static function connect()
 	{
-		$mysqli = new mysqli("localhost","root","nexus11235","sample");
+		$mysqli = new mysqli("localhost","gpiftp", "pass3232", "gpi");
 
 		if($mysqli->connect_errno)
 			echo "Fallo al conectar a MySQL: (".$mysqli->connect_errno.")".$mysqli->connect_error;
